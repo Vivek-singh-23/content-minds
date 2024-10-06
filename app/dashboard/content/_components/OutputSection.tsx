52f94e98fd1437765c8221ab5b9b8e1dac076214
Vivek-singh-23 <viveksinghy23@gmail.com>
2024-10-06 12:10:31 +0530

    Second commit - Added form and tesxt Editor

---
+++ app/dashboard/content/_components/OutputSection.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from "react";
+import "@toast-ui/editor/dist/toastui-editor.css";
+
+import { Editor } from "@toast-ui/react-editor";
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+
+function OutputSection() {
+    const editorRef:any=useRef()
+  return (
+    <div className="bg-white shagow-lg border rounded-lg">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg  ">Your Result</h2>
+        <Button className="flex gap-2">
+          <Copy />
+          Copy
+        </Button>
+      </div>
+      <Editor
+        ref={editorRef}
+        initialValue="Your result will appear here"
+        initialEditType="wysiwyg"
+        height="600px"
+        useCommandShortcut={true}
+        onChange={()=>console.log(editorRef.current.getInstance().getMarkdown())}
+      />
+    </div>
+  );
+}
+
+export default OutputSection;
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,7 +9,7 @@ function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
+    <div className="bg-slate-100 h-screen" >
       <div className="md:w-64 hidden md:block fixed">
         <SideNav />
       </div>

1564b63675fa32a6d8c22e971a28b18ce5a4a7e1
Vivek-singh-23 <viveksinghy23@gmail.com>
2024-10-08 18:55:18 +0530

    Debugging

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 function Settings() {
   return (
     <div className='flex items-center justify-center h-full'>
-        <UserProfile/>
+        <UserProfile routing="hash" />
     </div>
   )
 }
--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.tsx",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://neondb_owner:OC31ANBKsgyj@ep-shrill-mountain-a5x0xplf.us-east-2.aws.neon.tech/neondb?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
   };
